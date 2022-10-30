3f54141b3f7f4fb5aa643b3fd86b7b0a646faed6
Alexey Kudryavtsev <alveek@yandex.ru>
2022-10-30 19:15:33 +0300

    скрипт формы

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popupElem = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let nameInput = document.querySelector('.form__input_user_name');
+let jobInput = document.querySelector('.form__input_user_job');
+let formElem = document.querySelector('.form');
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
 
 function onOpen(popup) {
   popup.classList.add('popup_opened');
@@ -22,6 +30,16 @@ profileEditButton.addEventListener('click', () => {
   onOpen(popupElem);
 });
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  onClose(popupElem);
+}
+
 popupElem.addEventListener('click', function (event) {
   handlePopupDisplay(event);
 });
+
+formElem.addEventListener('click', formSubmitHandler);
