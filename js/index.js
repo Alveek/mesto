91b36816785770edde030003ee51fcd1e74f8352
Alexey Kudryavtsev <alveek@yandex.ru>
2022-10-30 16:31:39 +0300

    начал делать попап

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
-function hello(name) {
-  console.log(name);
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupElem = document.querySelector('.popup');
+
+function onOpen(popup) {
+  popup.classList.add('popup_opened');
 }
 
+function onClose(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function handlePopupDisplay(event) {
+  const isOverlay = event.target.classList.contains('popup_opened');
+  const isCloseButton = event.target.classList.contains('popup__close-button');
+
+  if (isOverlay || isCloseButton) {
+    onClose(popupElem);
+  }
+}
+
+profileEditButton.addEventListener('click', () => {
+  onOpen(popupElem);
+});
+
+popupElem.addEventListener('click', function (event) {
+  handlePopupDisplay(event);
+});
