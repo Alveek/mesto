6f1b0ddceaf262737bfc8d6abae667783ffc1aeb
Alexey Kudryavtsev <alveek@yandex.ru>
2022-12-30 04:32:39 +0300

    my brain exploded

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,18 +1,24 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, handleFormSubmit) {
+  // Кроме селектора попапа принимает в конструктор колбэк сабмита формы.
+  constructor(popupSelector, submitForm) {
     super(popupSelector);
   }
 
+  // Содержит приватный метод _getInputValues, который собирает данные всех полей формы.
   _getInputValues() {
 
   }
 
+  // Перезаписывает родительский метод setEventListeners. Метод setEventListeners класса
+  // PopupWithForm должен не только добавлять обработчик клика иконке закрытия,
+  // но и добавлять обработчик сабмита формы.
   setEventListeners() {
     super.setEventListeners();
   }
 
+  // Перезаписывает родительский метод close, так как при закрытии попапа форма должна ещё и сбрасываться.
   close() {
     super.close();
   }
