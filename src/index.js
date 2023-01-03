ae9e2471a0071a909078c45b7a24af9237d92b0c
alveek <alveek@gmail.com>
2023-01-04 00:30:40 +0300

    configure webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import {initialCards} from '../utils/cards-data.js';
-import {validationConfig} from '../utils/config.js';
-import FormValidator from '../components/FormValidator.js';
-import Card from '../components/Card.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import UserInfo from '../components/UserInfo.js';
-import PopupWithForm from '../components/PopupWithForm.js';
+import "./pages/index.css";
+
+import { initialCards } from "./utils/cards-data.js";
+import { validationConfig } from "./utils/config.js";
+import FormValidator from "./components/FormValidator.js";
+import Card from "./components/Card.js";
+import Section from "./components/Section.js";
+import PopupWithImage from "./components/PopupWithImage.js";
+import UserInfo from "./components/UserInfo.js";
+import PopupWithForm from "./components/PopupWithForm.js";
 
 import {
   profileEditButton,
@@ -21,38 +23,44 @@ import {
   formNewCard,
   cardsContainer,
   popupImagePreview,
-} from '../utils/constants.js';
+} from "./utils/constants.js";
 
 // Чтобы при открытии страницы не мелькали попапы
 // после загрузки содержимого у попапов удаляется класс скрывающий их
-window.addEventListener('DOMContentLoaded', () => {
-  popups.forEach((popup) => popup.classList.remove('popup_hidden'));
+window.addEventListener("DOMContentLoaded", () => {
+  popups.forEach((popup) => popup.classList.remove("popup_hidden"));
 });
 
 const profileFormValidator = new FormValidator(formProfile, validationConfig);
 const cardFormValidator = new FormValidator(formNewCard, validationConfig);
 const userInfo = new UserInfo(profileNameText, profileJobText);
-const popupWithImage = new PopupWithImage({popupSelector: popupImagePreview});
+const popupWithImage = new PopupWithImage({ popupSelector: popupImagePreview });
 
-const renderCard = new Section({
-  data: initialCards, renderer: (card) => {
-    createNewCard(card);
+const renderCard = new Section(
+  {
+    data: initialCards,
+    renderer: (card) => {
+      createNewCard(card);
+    },
   },
-}, cardsContainer,);
+  cardsContainer
+);
 
 const profileFormPopup = new PopupWithForm({
-  popupSelector: popupEditProfile, handleSubmitForm: (formData) => {
+  popupSelector: popupEditProfile,
+  handleSubmitForm: (formData) => {
     userInfo.setUserInfo(formData);
     profileFormPopup.close();
-  }
+  },
 });
 
 const newCardFormPopup = new PopupWithForm({
-  popupSelector: popupAddCard, handleSubmitForm: (formData) => {
-    const card = {name: formData.cardName, link: formData.cardLink};
+  popupSelector: popupAddCard,
+  handleSubmitForm: (formData) => {
+    const card = { name: formData.cardName, link: formData.cardLink };
     createNewCard(card);
     newCardFormPopup.close();
-  }
+  },
 });
 
 function addProfileDataToForm() {
@@ -65,19 +73,19 @@ function handleCardClick(name, link) {
 }
 
 function createNewCard(card) {
-  const newCard = new Card(card, '#card-template', handleCardClick);
+  const newCard = new Card(card, "#card-template", handleCardClick);
   const newCardElement = newCard.generateCard();
 
   renderCard.addItem(newCardElement);
 }
 
-profileEditButton.addEventListener('click', () => {
+profileEditButton.addEventListener("click", () => {
   addProfileDataToForm();
   profileFormValidator.resetValidation();
   profileFormPopup.open();
 });
 
-cardAddButton.addEventListener('click', () => {
+cardAddButton.addEventListener("click", () => {
   cardFormValidator.resetValidation();
   newCardFormPopup.open();
 });
